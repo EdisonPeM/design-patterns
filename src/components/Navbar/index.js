78a5427790e2d64cebb0fcdab92c6d18953ee01d
Edison Peñuela <edisonpe961206@hotmail.com>
2021-10-21 22:58:48 -0500

    Change Navbar Styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,9 @@
-import { NavLink as Link } from "react-router-dom";
+import NavLink from "../NavLink";
 
 import styles from "./styles.scss";
 
 const links = [
-  { path: "/", label: "Inicio" },
+  { path: "/", label: "Inicio", exact: true },
   { path: "/creacionales", label: "Creacionales" },
   { path: "/estructurales", label: "Estructurales" },
   { path: "/comportamentales", label: "Comportamentales" },
@@ -12,15 +12,16 @@ const links = [
 function Navbar() {
   return (
     <nav className={styles["navbar"]}>
-      {links.map(({ path, label }) => (
-        <Link
+      {links.map(({ path, label, exact }) => (
+        <NavLink
           key={path}
           to={path}
+          exact={exact}
           className={styles["navbar__link"]}
           activeClassName={styles["navbar__link--active"]}
         >
           {label}
-        </Link>
+        </NavLink>
       ))}
     </nav>
   );
